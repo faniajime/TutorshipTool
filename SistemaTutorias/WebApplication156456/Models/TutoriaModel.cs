9f836c1ebb84700c9f0b49a1f02603404d74048f
Fabiola Jiménez-González <45375479+faniajime@users.noreply.github.com>
2021-10-31 19:24:53 -0600

    Updated files so that it does not show conflicts

--- SistemaTutorias/WebApplication156456/Models/TutoriaModel.cs
+++ SistemaTutorias/WebApplication156456/Models/TutoriaModel.cs
@@ -7,13 +7,16 @@ namespace PruebaAzure.Models
 {
     public class Tutoria
     {
-        public string curso { get; set; }
-        public string tutor { get; set; }
+        public int tutoria_id { get; set; }
+        public int tutor_id { get; set; }
+
+        public string curso_id { get; set; }
+        public string nombre_curso { get; set; }
         public string tipo_sesion { get; set; }
         public int cantidad_estudiantes { get; set; }
         public int tarifa_individual { get; set; }
         public int tarifa_grupal { get; set; }
-        public int calificacion_tutoria { get; set; }
+        public double calificacion_tutoria { get; set; }
 
     }
 }
