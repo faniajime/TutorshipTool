37380dd96eeddc38a6ce0ba7c6b2bc74844d2659
Fabiola Jiménez-González <fabiola.jimenezgonzalez@ucr.ac.cr>
2021-12-09 23:19:18 +0000

    Fabi evaluar sesion

--- SistemaTutorias/WebApplication156456/Controllers/PerfilEstudianteController.cs
+++ SistemaTutorias/WebApplication156456/Controllers/PerfilEstudianteController.cs
@@ -15,20 +15,41 @@ namespace WebApplication156456.Controllers
     [Authorize(Roles = Roles.Estudiante)]
     public class PerfilEstudianteController : Controller
     {
-        private PerfilEstudianteHandler perfilEstudianteHandler;
-        private SesionHandler sesionHandler;
-        
-        public ActionResult Perfil()
+        private PerfilEstudianteHandler perfilEstudianteHandler = new PerfilEstudianteHandler();
+        private SesionHandler sesionHandler = new SesionHandler();
+        private MenuTutorHandler menuTutorHandler = new MenuTutorHandler();
+        public ActionResult Perfil(string thiserror = "false")
         {
-            perfilEstudianteHandler = new PerfilEstudianteHandler();
-            sesionHandler = new SesionHandler();
             int student_id = sesionHandler.getStudentID(User.Identity.GetUserId());
             //var model = perfilEstudianteHandler.getStudentSessions(User.Identity.GetUserId());
             //int my_id = Convert.ToInt32(User.Identity.GetUserId());
             List <Sesion> sesions = perfilEstudianteHandler.getStudentSessions(User.Identity.GetUserId());
+            ViewBag.thiserror = thiserror;
             ViewBag.sesions = sesions;
             ViewBag.studentID = student_id;
             return View();
+        }
+
+
+        public ActionResult Evaluar(int idSesion, string thiserror = "false")
+        {
+            Sesion sesion = sesionHandler.getSesion(idSesion);
+            ViewBag.estudianteID = sesionHandler.getStudentID(User.Identity.GetUserId());
+            ViewBag.sesion = sesion;
+            ViewBag.error = thiserror;
+            return View();
+        }
+
+        public ActionResult enviarEvaluacion(int sesionID, int estudianteID, int evaluacion)
+        {
+            //var yaevaluo = perfilEstudianteHandler.yaEvaluo(estudianteID, sesionID);
+           
+            perfilEstudianteHandler.evaluarSesion(sesionID, estudianteID, evaluacion);
+            menuTutorHandler.updateSessionRating(sesionID);
+            menuTutorHandler.updateTutorshipRatingFromSID(sesionID);
+            return RedirectToAction("Perfil", "PerfilEstudiante", new { thiserror = "true"});
+
+            
         }
     }
 }
